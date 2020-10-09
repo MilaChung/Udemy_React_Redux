c3e82e4683c166bd41f2bc603f06a053be2c27ce
Camila Chung <MilaChung@users.noreply.github.com>
2020-10-09 17:54:55 -0300

    [COMPONENTS] New React project created

---
+++ components/src/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return(
+        <div>
+            <h1>Components</h1>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+);
