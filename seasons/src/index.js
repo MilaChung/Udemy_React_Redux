b261e4b217e3fb327b881fb1aab9bff884fa4dba
Camila Chung <MilaChung@users.noreply.github.com>
2020-10-12 18:22:11 -0300

    Get the user's current position by its latitude

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,13 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-    return (
-        <div>
-            <h1> Seasons </h1>
+class App extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: " " };
+    }
+
+    componentDidMount() {
+        //navigator.geolocation.getCurrentPosition(sucess, error, options)
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage:err.message })
+        );
+    }
+
+    render() {
+        return (
+            <div>
+            <h1> The latitude is {this.state.lat} </h1>
         </div>
-    );
-};
+        );
+    }
+}
 
 ReactDOM.render(
     <App />,
