176c76de1f2223bbc3b288d777731b41dd957d4a
Camila Chung <MilaChung@users.noreply.github.com>
2020-10-12 21:09:13 -0300

    [PICS] New React project created

---
+++ pics/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return(
+        <div>
+            <h1> Pics </h1>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+);
