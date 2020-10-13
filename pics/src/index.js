f97d51eabe0451291f3832e9a17346d01365fd81
Camila Chung <MilaChung@users.noreply.github.com>
2020-10-12 22:05:06 -0300

    SearchBar added

--- pics/src/index.js
+++ pics/src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const App = () => {
-    return(
-        <div>
-            <h1> Pics </h1>
-        </div>
-    );
-}
+import App from "./components/App";
 
 ReactDOM.render(
     <App />,
