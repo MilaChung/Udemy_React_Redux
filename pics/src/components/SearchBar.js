755f37b5143f1f9929a8b9ac7744475c0173b5ec
Camila Chung <MilaChung@users.noreply.github.com>
2020-10-12 22:39:13 -0300

    Getting the term that the user entered

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,14 +1,28 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = { term: " " };
+
+    onInputChange = e => {
+        this.setState({ term: e.target.value })
+    }
+
+    onFormSubmit = e => {
+        e.preventDefault();
+        console.log(this.state.term);
+    }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label> Image Search </label>
-                        <input type="text"/>
+                        <input 
+                            type="text"
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
                     </div>
                 </form>
             </div>
