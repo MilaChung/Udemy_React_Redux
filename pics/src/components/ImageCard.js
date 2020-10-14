e8a9d554c9c9032d41fc07faea84ad42fb91ca8b
Camila Chung <MilaChung@users.noreply.github.com>
2020-10-14 17:21:21 -0300

    Style adjustments
    Each row of 10px was created and gap between the rows was eliminated

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -3,6 +3,9 @@ import React from "react";
 class ImageCard extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = { spans: 0 };
+
         this.imageRef = React.createRef();
     }
 
@@ -11,16 +14,22 @@ class ImageCard extends React.Component {
     }
 
     setSpans = () => {
-        console.log(this.imageRef.current.clientHeight);
+        const height = this.imageRef.current.clientHeight;
+
+        const spansRows = Math.ceil(height / 10);
+
+        this.setState({ spans: spansRows });
     }
 
     render () {
+        const {description, urls} = this.props.image;
+
         return (
-            <div>
+            <div style={{gridRowEnd: `span ${this.state.spans}`}}>
                 <img
                     ref={this.imageRef} 
-                    src={this.props.image.urls.regular}
-                    alt={this.props.image.alt_description}
+                    src={urls.regular}
+                    alt={description}
                 />
             </div>
         );
